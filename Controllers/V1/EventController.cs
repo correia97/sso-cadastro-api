a0eb15dd8d16fd9e4453b3631c3a0812da78bc6f
Paulo Eduardo Correia <correia97@gmail.com>
2023-08-31 13:11:11 -0300

    separação de projetos

---
+++ Controllers/V1/EventController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Cadastro.API.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult SendEvent<T>(T even)
+        {
+            return Ok(even);
+        }
+    }
+}
